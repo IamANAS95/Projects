a66fb36eaf585ae094194a86eb363997e4152553
Anas <iamanasx@gmail.com>
2021-09-08 14:08:50 +0300

    empty

---
+++ Price_Panel/User.java
@@ -0,0 +1,14 @@
+package Project.Price_Panel;
+
+public class User {
+    int userId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+}
